3e63d4418510e8ccff76b6078529c0a7728e35f2
Dylan Starink <dylan@starink.com>
2021-04-03 13:07:33 -0700

    Added Various Files

---
+++ src/main/java/frc/robot/hardware/RandomObscureMotor.java
@@ -0,0 +1,10 @@
+package frc.robot.hardware;
+
+public class RandomObscureMotor {
+    public void runMotor(double speed){
+        System.out.println("!!!RUN ERROR!!!");
+    }
+    public void stopMotor(){
+        System.out.println("!!!STOP ERROR!!!");
+    }
+}
